e0d3ccd259b5a08565edb61d786134630ba5d97e
AleksandrAlex <suslovalmi@mail.ru>
2020-05-15 22:39:22 +0300

    Load data in repository. Before hash in DB.

--- app/src/main/java/com/suslovalex/newsservice/repository/NewsRepository.kt
+++ app/src/main/java/com/suslovalex/newsservice/repository/NewsRepository.kt
@@ -4,15 +4,23 @@ import android.app.Application
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import com.google.gson.Gson
 import com.suslovalex.newsservice.data.NewsDAO
 import com.suslovalex.newsservice.data.NewsDB
 import com.suslovalex.newsservice.model.News
+import com.suslovalex.newsservice.retrofit.APINews
+import com.suslovalex.newsservice.retrofit.NewsClient
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
 
 class NewsRepository (application: Application ) {
-
+    private lateinit var disposable : Disposable
     private val newsDAO : NewsDAO
-    private var newsLiveData: LiveData<News>
+     private var newsLiveData : LiveData<News>
     private val newsDB: NewsDB = NewsDB.getInstance(application)
+    private var mNews: News? = null
 
     init {
         newsDAO = newsDB.newsDAO()
@@ -24,13 +32,50 @@ class NewsRepository (application: Application ) {
         return newsLiveData
     }
 
-    fun insert (news: News){
+    private fun insertNews (news: News){
         newsDB.newsDAO().insetNews(news)
         Log.d("insert", "${newsLiveData.value}")
     }
 
-    fun deleteAllNews (){
+    private fun deleteAllNews (){
         newsDB.newsDAO().deleteAllNews()
+    }
+
+    fun loadData(news: String) {
+        Log.d("load", news)
+        val retrofit = NewsClient.instance
+        Log.d("load", "$retrofit")
+        val articleRetrofit = retrofit.create(APINews::class.java)
+        var observable: Observable<News>? = null
+        when(news){
+            "TECHNOLOGY" -> observable = articleRetrofit.getTechnologyNews()
+            "SPORT" -> observable = articleRetrofit.getSportNews()
+            "SCIENCE" -> observable = articleRetrofit.getScienceNews()
+            "HEALTH" -> observable = articleRetrofit.getHealthNews()
+            "ENTERTAINMENT" -> observable = articleRetrofit.getEntertainmentNews()
+            else -> Log.d("tag", "error")
+        }
+        Log.d("load", "$observable")
+
+        disposable = observable!!
+            .subscribeOn(Schedulers.io())
+            //.delay(3, TimeUnit.SECONDS)
+            //.replay()
+//            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                deleteAllNews()
+                insertNews(it)
+//                newsLiveData.value = it
+                mNews = it
+
+                Log.d("load", "fun loadNewsToDataBase ${Gson().toJson(mNews)}")
+            }, {
+                //Toast.makeText(getApplication(),"ERROR!", Toast.LENGTH_LONG).show()
+            })
+        Log.d("load", "fun loadNewsToDataBase end2 ${Gson().toJson(mNews)}")
+
+
+//             disposable.dispose()
     }
 
 }
--- app/src/main/java/com/suslovalex/newsservice/viewmodel/NewsViewModel.kt
+++ app/src/main/java/com/suslovalex/newsservice/viewmodel/NewsViewModel.kt
@@ -1,88 +1,27 @@
 package com.suslovalex.newsservice.viewmodel
 
-
-import android.annotation.SuppressLint
 import android.app.Application
-import android.util.Log
-import android.widget.Toast
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import com.google.gson.Gson
-import com.suslovalex.newsservice.data.NewsDB
 import com.suslovalex.newsservice.model.News
 import com.suslovalex.newsservice.repository.NewsRepository
-import com.suslovalex.newsservice.retrofit.APINews
-import com.suslovalex.newsservice.retrofit.NewsClient
-import io.reactivex.Observable
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.disposables.Disposable
-import io.reactivex.schedulers.Schedulers
+
 
 class NewsViewModel(application: Application) : AndroidViewModel(application) {
 
-    private var thisNews = "TECHNOLOGY"
-    private lateinit var disposable : Disposable
-    private var mNews: News? = null
+    private var newsLiveData : LiveData<News>
 
     private val newsRepository: NewsRepository = NewsRepository(application)
-    private var news: LiveData<News>
 
     init {
-        news = newsRepository.getAllNews()
-        Log.d("viewModel_init_news", "${news.value}")
+        newsLiveData = newsRepository.getAllNews()
     }
 
     fun getNewsLiveData(): LiveData<News> {
-        return news
+        return newsLiveData
     }
 
     fun loadNewsToDataBase(selectedBlogNews: String) {
-        Log.d("Tag", "fun loadNewsToDataBase()")
-        thisNews = selectedBlogNews
-        Log.d("load", thisNews)
-        val retrofit = NewsClient.instance
-        Log.d("load", "$retrofit")
-        val articleRetrofit = retrofit.create(APINews::class.java)
-        var observable: Observable<News>? = null
-        when(thisNews){
-            "TECHNOLOGY" -> observable = articleRetrofit.getTechnologyNews()
-            "SPORT" -> observable = articleRetrofit.getSportNews()
-            "SCIENCE" -> observable = articleRetrofit.getScienceNews()
-            "HEALTH" -> observable = articleRetrofit.getHealthNews()
-            "ENTERTAINMENT" -> observable = articleRetrofit.getEntertainmentNews()
-            else -> Log.d("tag", "error")
-        }
-        Log.d("load", "$observable")
-
-        disposable = observable!!
-            .subscribeOn(Schedulers.io())
-//            .observeOn(Schedulers.newThread())
-            .subscribe({
-                mNews = it
-                removeAllNewsFromDataBase()
-                insertNewsToDataBase(it)
-//                setNews(it)
-
-                Log.d("load", "fun loadNewsToDataBase ${Gson().toJson(mNews)}")
-            }, {
-            })
-//            Log.d("load", "fun loadNewsToDataBase end1 ${mNews?.let { setNews(it) }}")
-            Log.d("load", "fun loadNewsToDataBase end2 ${Gson().toJson(mNews)}")
-
-    }
-
-    private fun insertNewsToDataBase(news: News) {
-        newsRepository.insert(news)
-    }
-
-    private fun removeAllNewsFromDataBase() {
-        newsRepository.deleteAllNews()
-    }
-
-    override fun onCleared() {
-        super.onCleared()
-        disposable.dispose()
+        newsRepository.loadData(selectedBlogNews)
     }
 }
